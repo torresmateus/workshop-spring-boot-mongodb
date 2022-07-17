d79d3bfb8e1a8f6e3b1bb275535b10f8e5a67aad
Mateus Torres <mateustorres2017@gmail.com>
2022-07-17 16:11:57 -0300

    Deleting user with DELETE

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -31,6 +31,12 @@ public class UserService {
 		return userRepository.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		
+		userRepository.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
